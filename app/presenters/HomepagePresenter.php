48f715e32da31b039b775e2ed4c317750f5f2304
Michal Vlček <vlcekmi3@fel.cvut.cz>
2014-11-08 21:55:45 +0100

    New room handling.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -11,16 +11,25 @@ use Nette,
  */
 class HomepagePresenter extends BasePresenter
 {
+	const ROOMS_URL = "http://jsonplaceholder.typicode.com/todos";
+	const MESSAGE_URL = "http://jsonplaceholder.typicode.com/todos";
 
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		// get all rooms
+		$req = new \Kdyby\Curl\Request(static::ROOMS_URL);
+		try {
+			$response = $req->get();
+			$this->template->rooms = \Nette\Utils\Json::decode($response->getResponse());
+		} catch (\Kdyby\Curl\CurlException $e) {
+			$this->error($e->getMessage(), Nette\Http\Response::S400_BAD_REQUEST);
+		}
 	}
 
 	public function handlePoll()
 	{
 		if ($this->isAjax()) {
-			// $test = new \Kdyby\Curl\Request("http://jsonplaceholder.typicode.com/todos");
+			// $test = new \Kdyby\Curl\Request(static::ROOMS_URL);
 			// try {
 			// 	$response = $test->get();
 			// 	$json = \Nette\Utils\Json::decode($response->getResponse());
@@ -47,13 +56,45 @@ class HomepagePresenter extends BasePresenter
 		}
 	}
 
-	public function handleCreateRoom()
+	/**
+	 * Creates new message
+	 */
+	public function handleMessage()
 	{
 		if ($this->isAjax()) {
-			$this->sendResponse(new Nette\Application\Responses\JsonResponse($this->context->httpRequest->getPost()));
+			$req = new \Kdyby\Curl\Request(static::MESSAGE_URL);
+			try {
+				$response = $req->post($this->context->httpRequest->getPost());
+				$this->sendResponse(new Nette\Application\Responses\JsonResponse(\Nette\Utils\Json::decode($response->getResponse())));
+			} catch (\Kdyby\Curl\CurlException $e) {
+				$this->error($e->getMessage(), Nette\Http\Response::S400_BAD_REQUEST);
+			}
 		}
 	}
 
+	/**
+	 * Creates new room
+	 */
+	public function handleCreateRoom()
+	{
+		if ($this->isAjax()) {
+			$req = new \Kdyby\Curl\Request(static::MESSAGE_URL);
+			try {
+				$response = $req->post($this->context->httpRequest->getPost());
+				$json = \Nette\Utils\Json::decode($response->getResponse());
+				$return = $this->context->httpRequest->getPost();
+				$return['url'] = $this->link('room!', $json->id);
+				$this->sendResponse(new Nette\Application\Responses\JsonResponse($return));
+			} catch (\Kdyby\Curl\CurlException $e) {
+				$this->error($e->getMessage(), Nette\Http\Response::S400_BAD_REQUEST);
+			}
+		}
+	}
+
+	/**
+	 * Switch the active room
+	 * @param  int $id of room
+	 */
 	public function handleRoom($id = null)
 	{
 		if ($this->isAjax()) {
--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,67 @@
-$(function(){
+$(function() {
+	waitForMsg();
+});
 
+function waitForMsg() {
+	$.ajax({
+		type: 'GET',
+		url: pollUrl,
+		async: true,
+		cache: false,
+	 
+		success: function(json) {
+			console.log(json);
+
+			if (json['mine']) {
+				var $newItem = $($('.media.item-template').clone()).removeClass('item-template');
+				$newItem.addClass(json['mine']);
+				$('#chat').append($newItem).fadeIn('slow');
+
+				$('html, body').animate({ scrollTop: $($newItem).offset().top + 20 }, 100);
+			}
+			setTimeout("waitForMsg()",2000);
+		},
+		error: function(XMLHttpRequest,textStatus,errorThrown) {
+			// alert("error: "+textStatus + " "+ errorThrown );
+			setTimeout("waitForMsg()",2000);
+		}
+	});
+}
+
+/* new room */
+$('#new-room').on('click', function(e) {
+	$.ajax({
+		type: 'POST',
+		url: $(e.target).attr('data-url'),
+		async: true,
+		cache: false,
+		data: {'name': $('#new-room-name').val()},
+		success: function(json) {
+			var $newItem = $($('.room.item-template').clone()).removeClass('item-template');
+			$newItem.find('a').html(json['name']); // name
+			$newItem.find('a').attr('href', json['url']); // url
+			$('#roomList').after($newItem);
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("error: "+textStatus + " "+ errorThrown );
+		}
+	});
+});
+
+$('.sidebar').on('click', '.room', function(e) {
+	e.preventDefault();
+
+	$.ajax({
+		type: 'GET',
+		url: $(e.target).attr('href'),
+		async: true,
+		cache: false,
+		// data: {'id': $('#new-room-name').val()},
+		success: function(json) {
+			console.log(json);
+		},
+		error: function(XMLHttpRequest,textStatus,errorThrown) {
+			// alert("error: "+textStatus + " "+ errorThrown );
+		}
+	});
 });
